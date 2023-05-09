6d7973e68353eae345ad7c053eebbd9e10ad7121
KaisuHa <kaisu_h@yahoo.com>
2023-05-09 15:17:19 +0300

    Lopullinen

--- App.js
+++ App.js
@@ -13,12 +13,12 @@ import Kartta from './Kartta';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
- 
+
   return (
 
     <NavigationContainer>
-       <Header backgroundColor= 'rgba(111, 195, 180, 1)'></Header>
-      <Tab.Navigator    
+      <Header backgroundColor='rgba(111, 195, 180, 1)'></Header>
+      <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
@@ -37,24 +37,24 @@ export default function App() {
               iconName = focused ? 'add-circle-outline' : 'add-circle-outline'
             }
 
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'rgba(111, 195, 180, 1)',
-            tabBarInactiveTintColor: 'black',
-            tabBarStyle: { 
-              backgroundColor: '#ffffff',
-              shadowOpacity: 0.70,
-              shadowColor: 'black' 
-}
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'rgba(111, 195, 180, 1)',
+          tabBarInactiveTintColor: 'black',
+          tabBarStyle: {
+            backgroundColor: '#ffffff',
+            shadowOpacity: 0.70,
+            shadowColor: 'black'
+          }
 
-          })}>
-          <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
-          <Tab.Screen name="Hotellit" component={Hotellit} options={{ headerShown: false }}  />
-          <Tab.Screen name="Kartta" component={Kartta} options={{ headerShown: false }} />
-        </Tab.Navigator>
-        
-      </NavigationContainer>
-  
+        })}>
+        <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Tab.Screen name="Hotellit" component={Hotellit} options={{ headerShown: false }} />
+        <Tab.Screen name="Kartta" component={Kartta} options={{ headerShown: false }} />
+      </Tab.Navigator>
+
+    </NavigationContainer>
+
   );
 }
 
@@ -66,5 +66,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
- 
+
 });
